e68fdfbe6790696805a6c5006d082c894710ccf2
kkmaple <kkmaple@qq.com>
2018-10-19 12:01:02 +0800

    #init

---
+++ pic-dir-rnd/Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace pic_dir_rnd
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(579, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "选择目录";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(249, 114);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(313, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(157, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "目标目录：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(294, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(225, 35);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "天机不可泄露";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(133, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "垃圾图片目录：";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(249, 180);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(313, 21);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(579, 177);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "选择目录";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("宋体", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.Location = new System.Drawing.Point(315, 374);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(167, 50);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "轰炸！！！";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Items.AddRange(new object[] {
+            "Apple",
+            "Bob",
+            "Cat",
+            "Dog",
+            "Ellipse",
+            "Fat",
+            "God",
+            "Hi",
+            "I",
+            "Jack",
+            "King",
+            "Lucy",
+            "Mars",
+            "Nurse",
+            "Ok",
+            "Pig",
+            "Queen",
+            "Rabbit",
+            "Sit",
+            "Tom",
+            "Uber",
+            "Voice",
+            "Wow",
+            "Xixi",
+            "You",
+            "Zoo"});
+            this.listBox1.Location = new System.Drawing.Point(148, 257);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 88);
+            this.listBox1.TabIndex = 8;
+            // 
+            // listBox2
+            // 
+            this.listBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Items.AddRange(new object[] {
+            "Apple",
+            "Bob",
+            "Cat",
+            "Dog",
+            "Ellipse",
+            "Fat",
+            "God",
+            "Hi",
+            "I",
+            "Jack",
+            "King",
+            "Lucy",
+            "Mars",
+            "Nurse",
+            "Ok",
+            "Pig",
+            "Queen",
+            "Rabbit",
+            "Sit",
+            "Tom",
+            "Uber",
+            "Voice",
+            "Wow",
+            "Xixi",
+            "You",
+            "Zoo"});
+            this.listBox2.Location = new System.Drawing.Point(337, 257);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 88);
+            this.listBox2.TabIndex = 9;
+            // 
+            // listBox3
+            // 
+            this.listBox3.BackColor = System.Drawing.SystemColors.Window;
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 12;
+            this.listBox3.Items.AddRange(new object[] {
+            "Apple",
+            "Bob",
+            "Cat",
+            "Dog",
+            "Ellipse",
+            "Fat",
+            "God",
+            "Hi",
+            "I",
+            "Jack",
+            "King",
+            "Lucy",
+            "Mars",
+            "Nurse",
+            "Ok",
+            "Pig",
+            "Queen",
+            "Rabbit",
+            "Sit",
+            "Tom",
+            "Uber",
+            "Voice",
+            "Wow",
+            "Xixi",
+            "You",
+            "Zoo"});
+            this.listBox3.Location = new System.Drawing.Point(534, 257);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(120, 88);
+            this.listBox3.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(298, 297);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(11, 12);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "+";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(490, 297);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(11, 12);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "+";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(364, 225);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 12);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "随机文件名";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "垃圾图片添加器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ pic-dir-rnd/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pic_dir_rnd
+{
+    public partial class Form1 : Form
+    {
+        FileInfo[] fil;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog2.ShowDialog() == DialogResult.OK)
+                textBox2.Text = folderBrowserDialog2.SelectedPath;
+        }
+
+        /// <summary>
+        /// 获取垃圾图片文件
+        /// </summary>
+        /// <param name="path">垃圾图片目录路径</param>
+        void getJunkFiles(string path)
+        {
+            DirectoryInfo dir = new DirectoryInfo(path);
+
+            fil = dir.GetFiles();
+
+            var length = fil.Length;
+
+            int index = new Random().Next(0, length);
+            MessageBox.Show(index.ToString());
+            MessageBox.Show(fil[index].FullName);
+        }
+
+        /// <summary>
+        /// 随机复制垃圾图片
+        /// </summary>
+        /// <param name="path">要复制垃圾图片的目录</param>
+        void copyRandomFiles(string path)
+        {
+            //开始工作
+            //随机要添加的垃圾数量
+            int num = new Random().Next(6, 777);
+
+            for(int i = 0; i < num; ++i)
+            {
+                var length = fil.Length;
+                //随机文件名
+                string rndFileName = listBox1.GetItemText(listBox1.Items[new Random().Next(0, listBox1.Items.Count)]) + 
+                    listBox2.GetItemText(listBox2.Items[new Random().Next(0, listBox2.Items.Count)]) + 
+                    listBox3.GetItemText(listBox3.Items[new Random().Next(0, listBox3.Items.Count)]) +
+                    (new Random().Next(0, 100) % 2 == 0 ? ".png" : ".jpg");
+                //如果不存在就丢一个过去
+                if(!File.Exists(Path.Combine(path,rndFileName)))
+                    File.Copy(fil[new Random().Next(0, length)].FullName,Path.Combine(path, rndFileName));
+            }
+
+            DirectoryInfo dir = new DirectoryInfo(path);
+
+            DirectoryInfo[] dii = dir.GetDirectories();
+
+            foreach (DirectoryInfo d in dii)
+
+            {
+                copyRandomFiles(d.FullName);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(!Directory.Exists(textBox1.Text))
+            {
+                MessageBox.Show("请选择正确的目标目录！");
+                return;
+            }
+
+            if (!Directory.Exists(textBox2.Text))
+            {
+                MessageBox.Show("请选择正确的垃圾图片目录！");
+                return;
+            }
+
+            getJunkFiles(textBox2.Text);
+
+            textBox1.ReadOnly = true;
+            textBox2.ReadOnly = true;
+
+            copyRandomFiles(textBox1.Text);
+
+            textBox1.ReadOnly = false;
+            textBox2.ReadOnly = false;
+        }
+    }
+}
